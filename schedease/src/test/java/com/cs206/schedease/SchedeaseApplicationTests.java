e68072a80587cd4ed8ca61c80492cea2eda85ab8
Li Zhan Wesly Chau <wesly.chau.li.zhan.01@gmail.com>
2024-02-26 17:00:04 +0800

    set up repo for backend app that calls google calendar api

---
+++ schedease/src/test/java/com/cs206/schedease/SchedeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs206.schedease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
