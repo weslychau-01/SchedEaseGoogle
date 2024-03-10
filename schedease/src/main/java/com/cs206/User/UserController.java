55c90e1e97c269f4f6c97a140ab0a5a8c1528c88
Li Zhan Wesly Chau <wesly.chau.li.zhan.01@gmail.com>
2024-03-10 17:47:56 +0800

    updated controller method for adding user, for testing purposes

--- schedease/src/main/java/com/cs206/User/UserController.java
+++ schedease/src/main/java/com/cs206/User/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,20 +20,25 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/getUsers")
-    public ResponseEntity<?> getAllUsers(){
+    public ResponseEntity<?> getAllUsers() {
         List<User> allUsers = userService.allUsers();
-        if(allUsers == null){
+        if (allUsers == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user not found");
         }
         return new ResponseEntity<List<User>>(userService.allUsers(), HttpStatus.OK);
     }
-//    @PostMapping("/addUsers")
-//    public ResponseEntity<String> createUser(){
-//        List<String> eventId = new ArrayList<>();
-//        User user = new User();
-//        user.setName("John");
-//        user.setEventId();
-//
-//        return new ResponseEntity<> ("User Saved", HttpStatus.OK);
-//    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/addUsers")
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        List<String> eventId = new ArrayList<>();
+        // User user = new User();
+        // user.setUserName("John");
+        // user.setUserEmail("john@gmail.com");
+        // user.setUserPassword("john123");
+        user.setUserEventIds(eventId);
+        user.setUserMeetingIds(new ArrayList<String>());
+        userService.save(user);
+        return new ResponseEntity<>("User Saved", HttpStatus.OK);
+    }
 }
